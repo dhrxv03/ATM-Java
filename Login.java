b8a899601811bc5ba6a561067ddb85a552325756
dhrxv03 <dhruv0310@icloud.com>
2021-12-21 13:18:52 +0530

    First Commit

---
+++ Login.java
@@ -0,0 +1,101 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.WindowConstants;
+
+public class Login extends JFrame {
+
+	String pin = "1234";
+	int attempts = 3;
+
+	public Login() {
+		
+		super("Login Screen");
+		setLayout(new BorderLayout());
+		setResizable(false);
+		setLocationRelativeTo(null);
+		buildApp();
+		pack();
+		setSize(250, 110);
+		setVisible(true);
+		setDefaultLookAndFeelDecorated(true);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new Login();
+	}
+
+	void buildApp() {
+		
+		JLabel pinInstruction = new JLabel("Please enter your pin number");
+		JPasswordField pinText = new JPasswordField(4);
+		JButton entPin = new JButton("Enter");
+		JButton quit = new JButton("Quit");
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		buttonPanel.add(quit);
+		buttonPanel.add(entPin);
+		pinText.setBackground(Color.white);
+		add(pinInstruction, BorderLayout.NORTH);
+		add(pinText, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
+
+		
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		});
+
+		pinText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				char[] pinGuess = pinText.getPassword();
+				String pinString = new String(pinGuess);
+				if (pinString.equals(pin)) {
+					JOptionPane.showMessageDialog(null, "That pin is correct! Opening Account...");
+					dispose();
+					new ATM();
+				} else {
+					if (attempts != 1) {
+						attempts--;
+						JOptionPane.showMessageDialog(null,
+								"That pin is incorrect! \n" + attempts + " attempts remaining!");
+					} else {
+						JOptionPane.showMessageDialog(null, "No Attempts remaining! \n Closing Program");
+						System.exit(0);
+					}
+				}
+			}
+		});
+		entPin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				char[] pinGuess = pinText.getPassword();
+				String pinString = new String(pinGuess);
+				if (pinString.equals(pin)) {
+					JOptionPane.showMessageDialog(null, "That pin is correct! Opening Account...");
+					dispose();
+					new ATM();
+				} else {
+					if (attempts != 1) {
+						attempts--;
+						JOptionPane.showMessageDialog(null,
+								"That pin is incorrect! \n" + attempts + " attempts remaining!");
+					} else {
+						JOptionPane.showMessageDialog(null, "No Attempts remaining! \n Closing Program");
+						System.exit(0);
+					}
+				}
+			}
+		});
+	}
+}
